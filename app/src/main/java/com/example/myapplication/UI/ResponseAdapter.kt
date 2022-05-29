08bb3db1568ee100a3aa159386f3132d275bfe9b
Nishant Rishab <75352438+im-reprisal@users.noreply.github.com>
2022-05-29 14:54:36 +0530

    commit

--- app/src/main/java/com/example/myapplication/UI/ResponseAdapter.kt
+++ app/src/main/java/com/example/myapplication/UI/ResponseAdapter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.DATA.Item
 import com.example.myapplication.databinding.ItemLayoutBinding
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ResponseAdapter(private val context: Context, private val responseList : List<Item>) : RecyclerView.Adapter<ResponseViewHolder>(){
@@ -22,7 +26,19 @@ class ResponseAdapter(private val context: Context, private val responseList : L
     override fun onBindViewHolder(holder:ResponseViewHolder, pos: Int) {
         val data = responseList[pos]
         holder.itemLayoutBinding.apply {
+            val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            sdf.timeZone = TimeZone.getTimeZone("GMT")
+            try {
+                val time: Long = sdf.parse(data.snippet.publishedAt).time
+                val now = System.currentTimeMillis()
+                val ago = DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS)
+                publishedTime.text = ago
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
+
             videoTitle.text = data.snippet.title
+            channelName.text = data.snippet.channelTitle
             Glide.with(context).load(data.snippet.thumbnails.high.url).into(videoThumbnail)
             Glide.with(context).load(data.snippet.thumbnails.high.url).into(channelPicture)
         }
