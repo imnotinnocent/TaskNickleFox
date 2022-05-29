1a9cc4dcfd61dd73a120a5a2abaad83a353e1612
Nishant Rishab <75352438+im-reprisal@users.noreply.github.com>
2022-05-29 16:07:12 +0530

    commit

--- app/src/main/java/com/example/myapplication/API/Service.kt
+++ app/src/main/java/com/example/myapplication/API/Service.kt
@@ -4,10 +4,13 @@ import com.example.myapplication.DATA.Item
 import com.example.myapplication.DATA.ResponseModel
 import com.example.myapplication.DATA.Snippet
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Service {
-    @GET("search?part=snippet&q=the weekend&key=AIzaSyBydQ2cs7uwj1FcTxRuvFFZ7GPO3QcK2tM")
+    @GET("search")
     suspend fun getAllResponse(
-
+        @Query("part") part:String,
+        @Query("q") q:String,
+        @Query("key") key:String
     ) : ResponseModel
 }
