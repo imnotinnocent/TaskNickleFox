aa5aee1a3df927df9c36265fa585e5327e7b84d8
Nishant Rishab <75352438+im-reprisal@users.noreply.github.com>
2022-05-29 17:59:29 +0530

    commit

---
+++ app/src/main/java/com/example/myapplication/UI/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.UI
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.myapplication.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }, 1000)
+    }
+}
